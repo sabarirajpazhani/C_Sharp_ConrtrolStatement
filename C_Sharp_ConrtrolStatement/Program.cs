1cf10d47cb5f34e6f2a5803c9ae71fba4be6ca4f
sabarirajp <sabarirajpazhani@gmail.com>
2025-04-24 11:33:12 +0530

    Add project files.

---
+++ C_Sharp_ConrtrolStatement/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace C_Sharp_ControlStatement
+{
+    public class Program
+    {
+        public static void SwitchStatement()
+        {
+            Console.WriteLine("Enter Your the Number to Get Lucky Tickets: ");
+            int id = Convert.ToInt32(Console.ReadLine());
+
+            switch (id)
+            {
+                case 1:
+                    Console.WriteLine("Hello... You Got the IPL Ticket");
+                    goto default;
+                case 2:
+                    Console.WriteLine("Hello... You Got the Movie Ticket");
+                    goto default;
+                case 3:
+                    Console.WriteLine("Hey There.... You Got the Doni Fan Meet Ticket");
+                    goto default;
+
+                default:
+                    Console.WriteLine("Your Entered Lucky Number is :" + id);
+                    break;
+            }
+        }
+        static void Main(String[] args)
+        {
+            SwitchStatement();
+        }
+    }
+}
